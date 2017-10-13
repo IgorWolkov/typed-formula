faf68c22a50b15507ca8e03d08561b6767368839
Maksim Ochenashko <maksim.ochenashko@external.kreditech.com>
2017-10-13 18:00:33 +0300

    Added @entity annotation

--- src/test/scala/karazinscalausersgroup/typed/formula/numbers/entities/EarlyRepaymentFee.scala
+++ src/test/scala/karazinscalausersgroup/typed/formula/numbers/entities/EarlyRepaymentFee.scala
@@ -1,20 +1,15 @@
 package karazinscalausersgroup.typed.formula.numbers.entities
 
+import karazinscalausersgroup.typed.formula.generic.entity
 import karazinscalausersgroup.typed.formula.numbers._
 
 /**
   * @author Igor Wolkov
   */
-// TODO: Add macros to generate this boilerplate
+@entity
 case class EarlyRepaymentFee(amount: Number, expressions: List[Covering] = Nil)
   extends InstallmentEntity {
 
-  override type Self = EarlyRepaymentFee
-
-  def copyEntity(): Self = copy()
-
-  def copyEntities(expressions: List[Covering]): Self = copy(amount, expressions)
-
-  // TODO: Rewrite, add additional information
   override def toString = s"EarlyRepaymentFee(totalAmount: $v, amount: $amount, coverings: ${ expressions mkString ", " })"
+
 }
